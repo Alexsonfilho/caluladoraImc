c7be501877fe73e56f265d4a1b95fab5c710509f
alexson <filhoalexson.filho@gmail.com>
2025-07-28 22:52:48 -0400

    Adicionar tabela de classificação de IMC com destaque dinâmico via DOM

--- scripts.js
+++ scripts.js
@@ -14,21 +14,33 @@ function calcularImc () {
     if (imc < 18.5) {
         msg = `Olá, senhor(a): ${nome}. Seu IMC é de ${imc}KG/M², classificação: Magreza.`;
         exibirMensagem("p", msg);
+
+        document.getElementById("magreza").classList.add("destacado-vermelho");
     } else if (imc >= 18.5 && imc < 25) {
         msg = `Olá, senhor(a): ${nome}. Seu IMC é de ${imc}KG/M², classificação: Normal.`;
         exibirMensagem("p", msg);
+
+        document.getElementById("normal").classList.add("destacado-verde");
     } else if (imc >= 25 && imc < 30) {
         msg = `Olá, senhor(a): ${nome}. Seu IMC é de ${imc}KG/M², classificação: Sobrepeso.`;
         exibirMensagem("p", msg);
+
+        document.getElementById("sobrepeso").classList.add("destacado-vermelho");
     } else if (imc >= 30 && imc < 35) {
         msg = `Olá, senhor(a): ${nome}. Seu IMC é de ${imc}KG/M², classificação: Obesidade I.`;
         exibirMensagem("p", msg);
+
+        document.getElementById("obesidade1").classList.add("destacado-vermelho");
     } else if (imc >= 35 && imc < 40) {
         msg = `Olá, senhor(a): ${nome}. Seu IMC é de ${imc}KG/M², classificação: Obesidade II.`;
         exibirMensagem("p", msg);
+
+        document.getElementById("obesidade2").classList.add("destacado-vermelho");
     } else {
         msg = `Olá, senhor(a): ${nome}. Seu IMC é de ${imc}KG/M², classificação: Obesidade III.`;
         exibirMensagem("p", msg);
+
+        document.getElementById("obesidade3").classList.add("destacado-vermelho");
     }
 
 
